567e210f47efead014d1b7278ed8f45b34635cb3
Ryan Jewell <ryanjewell@eflexsystems.com>
2019-08-21 15:19:27 -0400

    Adding Components for Template Building

--- _eflex/dist/script/script.js
+++ _eflex/dist/script/script.js
@@ -67,3 +67,11 @@ $(function(){
     $(this).toggleClass('active');
   })
 })
+
+// Brightness Toggle
+$(function(){
+  $('.brightness-toggle').click(function(e) {
+    $('.flex-fill').toggleClass('bg-black');
+    $('.font-italic').toggleClass('text-gray-light');
+  })
+})
