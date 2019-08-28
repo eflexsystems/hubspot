59f4d5eb27a124e3347d0743b5054e8e72135333
Ryan Jewell <ryanjewell@eflexsystems.com>
2019-08-28 11:27:21 -0400

    Some big changes

--- _eflex/dist/script/script.js
+++ _eflex/dist/script/script.js
@@ -59,19 +59,4 @@ if (hasTouch()) {
             }
         }
     } catch (ex) {}
-}
-
-// Tab Card Active
-$(function(){
-  $('.tab-card').click(function(e) {
-    $(this).toggleClass('active');
-  })
-})
-
-// Brightness Toggle
-$(function(){
-  $('.brightness-toggle').click(function(e) {
-    $('.flex-fill').toggleClass('bg-black');
-    $('.font-italic').toggleClass('text-gray-light');
-  })
-})
+  }
