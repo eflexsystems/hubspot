60cbaf6ec00a0426be5b43446d9030d950088648
Ryan Jewell <ryanjewell@eflexsystems.com>
2019-12-10 15:13:24 -0500

    navbar changes

---
+++ _eflex/dist/script/navbar.js
@@ -0,0 +1,7 @@
+$("#global_header .nav-link").mouseenter(function() {
+  $( this ).css('background-color', '#2f9ef8');
+});
+
+$("#global_header .nav-link").mouseleave(function() {
+  $( this ).css('background-color', '#f2f2f2');
+});
