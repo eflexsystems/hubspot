1333e9711bae4996a011b044a61f66b27aacf577
Ryan Jewell <ryanjewell@eflexsystems.com>
2021-05-26 14:07:03 -0400

    added gray-800 to body

--- dev/pages/platform/script.js
+++ dev/pages/platform/script.js
@@ -1,3 +1,21 @@
-window.addEventListener('scroll', () => {
-	document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+// window.addEventListener('scroll', () => {
+// 	document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+// }, false);
+
+LottieInteractivity.create({
+	player: '#lottie',
+	container: '#lottieContainer',
+	mode: 'scroll',
+	actions: [
+		{
+			visibility: [0, 0.28],
+			type: "stop",
+			frames: [0]
+		},
+		{
+			visibility: [0.28, 1.0],
+			type: 'seek',
+			frames: [0, 300],
+		},
+	],
+  });
