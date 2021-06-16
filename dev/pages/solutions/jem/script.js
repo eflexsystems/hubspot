f023c3cdb2e6f752e9913977b5bbfe9503a20906
Ryan Jewell <ryanjewell@Ryans-MacBook-Pro.local>
2021-06-16 09:17:30 -0400

    6.16.21

--- dev/pages/solutions/jem/script.js
+++ dev/pages/solutions/jem/script.js
@@ -1,1 +1,36 @@
-
+$('#carousel-jem').slick({
+    lazyLoad: 'ondemand',
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 5,
+    slidersToScroll: 1,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    speed: 400,
+    responsive: [
+        {
+            breakpoint: 992, // bootstrap lg
+            settings: {
+                arrows: true,
+                centerMode: true,
+                slidesToShow: 3,
+                infinite: true,
+                autoplay: true,
+                speed: 400
+            }
+        },
+        {
+            breakpoint: 576, // bootstrap sm
+            settings: {
+                arrows: true,
+                centerMode: false,
+                slidesToShow: 1,
+                infinite: true,
+                autoplay: true,
+                speed: 400
+            }
+        }
+    ]
+});
--- dist/pages/platform/script.js
+++ dist/pages/platform/script.js
@@ -1,21 +1,36 @@
-// window.addEventListener('scroll', () => {
-// 	document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-// }, false);
+window.addEventListener('scroll', () => {
+	document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
 
 LottieInteractivity.create({
+	mode: 'scroll',
 	player: '#lottie',
 	container: '#lottieContainer',
-	mode: 'scroll',
 	actions: [
 		{
-			visibility: [0, 0.28],
+			visibility: [0, 0.125],
 			type: "stop",
 			frames: [0]
 		},
 		{
-			visibility: [0.28, 1.0],
-			type: 'seek',
-			frames: [0, 300],
-		},
+			visibility: [0.125, 0.95],
+			type: "seek",
+			frames: [0, 810]
+		}
 	],
-  });
+});
+
+const instance = basicLightbox.create(`
+	<div>
+		<video controls data-id="2">
+		<source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4">
+	</div>
+`)
+
+document.querySelector('button.btn-video1').onclick = () => {
+	instance.show();
+}
+
+document.querySelector('button.btn-video2').onclick = () => {
+	instance.show();
+}
